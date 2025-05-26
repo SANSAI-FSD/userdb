43e09a7c6bec0d5447aec94d67891b8bdd3b4d59
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-05-26 13:27:40 +0530

    modfy

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  dob: { type: String, required: true },
-  sex: { type: String, required: true },
+  biodata: { type: String, required: true },
+  jobRole: { type: String, required: true },
 });
 
 const User = mongoose.model("Post", userSchema);
