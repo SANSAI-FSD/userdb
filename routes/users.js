1c6d9092fa2056a5a7da5159f95840cf17c1db9f
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-06-02 12:07:35 +0530

    originall

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,6 @@ const router = express.Router();
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, unique + ext);
-  },
-});
-
-const upload = multer({ storage });
-
-
 router.get("/api/users/", async (req, res) => {
   try {
     const { limit } = req.query;
